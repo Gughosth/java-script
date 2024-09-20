14b298e2c72f292b221e96721c0ef5dbfb7b6b36
Gughosth <gustavomazzini680@gmail.com>
2024-09-19 22:59:49 -0300

    atualized

---
+++ EX01/JS/index.js
@@ -0,0 +1,13 @@
+document.getElementById("texto").innerHTML=
+"Meu primeiro texto <b>JS</b>!";
+alert('ola ola');
+console.log("ola isso é um console log");
+
+
+
+let a,b,c; a=1;b=2;c=3;
+
+
+function minhafunção()(
+    
+)
