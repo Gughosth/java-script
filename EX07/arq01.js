d5de92a212aea225b83e223fc6d5ef06cf9e639a
Gughosth <gustavomazzini680@gmail.com>
2024-10-14 10:50:40 -0300

    comits

---
+++ EX07/arq01.js
@@ -0,0 +1,12 @@
+var vel = '50'
+
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+
+if (vel > 80){ // Condição simples : verdadeira 
+    console.log(`Caso ultrapassar o limite de velocidade estará : Multado`)
+
+}
+
+
+
+console.log(`Dirija sempre com cudado respeitando as regra de transito nacional para garantir sua segurança e a dos que estão a sua volta`)
---
+++ EX07/arq03.js
@@ -0,0 +1,11 @@
+var idade = 25
+console.log('Voce tem ${idade} anos.')
+if (idade < 16) {
+    console.log('Não vota ')
+} else if(idade < 18) {
+    console.log('voto opcinal')
+
+} else {
+    console.log('voto obrigatório')
+}
+    
